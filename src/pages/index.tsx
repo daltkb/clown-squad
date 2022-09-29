31c0cd7b7d060e344e347f326eace903fe4cfe56
daltkb <daltbenn@gmail.com>
2022-09-29 09:48:52 -0500

    product price and name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,7 +26,19 @@ export default function Home({ prices }: IProps) {
   return (
     <div>
       {prices.map((price) => (
-        <h1 key={price.id}>{price.product.name}</h1>
+        <>
+          <img
+            src={price.product.images[0]}
+            alt={price.product.name}
+            className="h-64 w-64"
+          />
+          <div className="flex items-center">
+            <h1 className="text-xl" key={price.id}>
+              {price.product.name}
+            </h1>
+            <p>${(price.unit_amount as number) / 100}</p>
+          </div>
+        </>
       ))}
     </div>
   );
